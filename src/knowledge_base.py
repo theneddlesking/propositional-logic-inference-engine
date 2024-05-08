ab5e8877f6816a4621cbb5657b2dc3af7c6bfb91
achuchuva <67221090+achuchuva@users.noreply.github.com>
2024-05-08 11:02:59 +1000

    Add propositional symbols to KB

--- src/knowledge_base.py
+++ src/knowledge_base.py
@@ -1,13 +1,16 @@
 from src.syntax.sentence import Sentence
+from src.syntax.proposition_symbol import PropositionSymbol
 
 
 class KnowledgeBase:
     # TODO implement knowledge base, figure out what properties and methods are needed
-    def __init__(self, sentences: list[Sentence] = None):
+    def __init__(self, propositional_symbols: dict[str, PropositionSymbol] = None, sentences: list[Sentence] = None):
+        self.propositional_symbols = propositional_symbols
         self.sentences = sentences if sentences is not None else []
 
     @classmethod
     def from_string(cls, string: str) -> 'KnowledgeBase':
+        propositional_symbols = {}
         
         # remove all spaces in the string
         string = string.replace(" ", "")
@@ -22,9 +25,9 @@ class KnowledgeBase:
         sentences = [sentence for sentence in sentences if sentence != ""]
 
         # get the actual sentences
-        sentences = [Sentence.from_string(sentence) for sentence in sentences]
+        sentences = [Sentence.from_string(sentence, propositional_symbols) for sentence in sentences]
 
-        return cls(sentences)
+        return cls(sentences, propositional_symbols)
     
     def __str__(self):
         return "\n".join([str(sentence) for sentence in self.sentences])
