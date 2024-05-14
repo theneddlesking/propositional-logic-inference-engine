8523f82f466f9d63547bb6b9be298a075805d305
achuchuva <67221090+achuchuva@users.noreply.github.com>
2024-05-14 18:28:11 +1000

    Stub in functions for truth table algorithm

--- src/algorithm/truth_table_checking.py
+++ src/algorithm/truth_table_checking.py
@@ -1,6 +1,14 @@
+from src.horn_knowledge_base import HornKnowledgeBase
 from src.inference_algorithm import InferenceAlgorithm
+from src.query import HornKnowledgeBaseQuery
+from src.result.chaining_result import ChainingResult
+from src.syntax.literal import PositiveLiteral
 
 # TODO implement the TruthTableChecking class
 class TruthTableChecking(InferenceAlgorithm):
     def __init__(self):
         super().__init__("TT")
+
+    def run(self, knowledge_base: HornKnowledgeBase, query: HornKnowledgeBaseQuery) -> ChainingResult:
+        pass
+    
